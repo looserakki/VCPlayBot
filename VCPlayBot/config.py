fc97ebe29d8f49e94f3a5f8a66f2721e192bcdd4
kishannn07 <84447786+kishannn07@users.noreply.github.com>
2021-06-19 16:54:11 +0530

    Update config.py

--- VCPlayBot/config.py
+++ VCPlayBot/config.py
@@ -8,7 +8,7 @@ if os.path.exists("local.env"):
 
 que = {}
 SESSION_NAME = getenv("SESSION_NAME", "BQCWVGfB8QKSDUQDF9eKYXnBvlX1LiMUI5CMTWwjqhZ0cCgHg-17tmAvOuSeTY2fIqT8PzDkLgCCiGrKZ74yo0WOHPVf0Rs6pVO8VEMk0uSsHYeMdR7OQJyZc--xWkrCeQXlmE-hIzOPFbB4jQLYUGss_eULEy98V1tnEw-qsX6wZ-jw8dkDhwEq-EJd7Vwvv7bAEAVYUbH_XLso9VdB6toYRCjneHy-U6q5SfCmUxxHA9tgBATq34aNySf4JG-FJu9AWTzMgrDpjW7FucOCoCZwA4xeZxwGPQUEvIx6pgn1Q0CsiXNuSmAKlRKsX-D5Posqnbhf2r0Cl4W-XMKfAnFeb6crEAA")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
+BOT_TOKEN = getenv("BOT_TOKEN", "1728730929:AAGdj_UpflyimraBPr-MRHi8PtVMwKZMWr4")
 BOT_NAME = getenv("BOT_NAME", "Patricia")
 UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "patricia_updates")
 BG_IMAGE = getenv("BG_IMAGE", "https://telegra.ph/file/c6d0abe29eddeaef32ac7.jpg")
