9c84fb55024d2db14b3d603f7179637b2850f1e2
kishannn07 <84447786+kishannn07@users.noreply.github.com>
2021-06-29 17:43:16 +0530

    Update chat_member_updated.py

--- VCPlayBot/modules/chat_member_updated.py
+++ VCPlayBot/modules/chat_member_updated.py
@@ -1,27 +1,25 @@
-from pyrogram import Client
 from pyrogram.types import ChatMemberUpdated
-
-from VCPlayBot.function import *
-
+from cache.admins import admins
+from pyrogram import Client
 
 @Client.on_chat_member_updated()
-async def chat_member_updated(_, chat_member_updated: ChatMemberUpdated):
-    if chat_member_updated.new_chat_member and chat_member_updated.old_chat_member:
+async def auto_admin_updater(_, chat_member_updated: ChatMemberUpdated):
+    (
+        admins[chat_member_updated.chat.id].append(
+            chat_member_updated.new_chat_member.user.id
+        )
+    ) if (
         (
-            admins.admins[chat_member_updated.chat.id].append(
-                chat_member_updated.new_chat_member.user.id,
-            )
-        ) if (
-            (chat_member_updated.new_chat_member.can_manage_voice_chats)
-            and (
-                (chat_member_updated.new_chat_member.user.id)
-                not in admins.admins[chat_member_updated.chat.id]
-            )
-        ) else (
-            admins.admins[chat_member_updated.chat.id].remove(
-                chat_member_updated.new_chat_member.user.id,
-            )
-        ) if (
-            (chat_member_updated.new_chat_member.user.id)
-            in admins.admins[chat_member_updated.chat.id]
-        ) else None
+            (
+                chat_member_updated.new_chat_member.user.id
+            ) not in admins[chat_member_updated.chat.id]
+        )
+    ) else (
+        admins[chat_member_updated.chat.id].remove(
+            chat_member_updated.new_chat_member.user.id
+        )
+    ) if (
+        (
+            chat_member_updated.new_chat_member.user.id
+        ) in admins[chat_member_updated.chat.id]
+    ) else None
